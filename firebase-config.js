2833f0e50a2a39c475bf6848c5a09ab123c4e686
debashreeemalik <debashreee.malik@gmail.com>
2025-12-17 02:08:40 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,20 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAUit5I8Qml9wWIsKATJkwOhOdlCw06irg",
-  authDomain: "slangaddaai.firebaseapp.com",
-  projectId: "slangaddaai",
-  storageBucket: "slangaddaai.firebasestorage.app",
-  messagingSenderId: "633159892736",
-  appId: "1:633159892736:web:b33c2d89f3cba1f609fde5",
-  measurementId: "G-LG4J2DX0H9"
-};
+<!-- firebase.js -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage();
+  const firebaseConfig = {
+    apiKey: "AIzaSyAUit5I8Qml9wWIsKATJkwOhOdlCw06irg",
+    authDomain: "slangaddaai.firebaseapp.com",
+    projectId: "slangaddaai",
+    storageBucket: "slangaddaai.firebasestorage.app",
+    messagingSenderId: "633159892736",
+    appId: "1:633159892736:web:b33c2d89f3cba1f609fde5",
+    measurementId: "G-LG4J2DX0H9"
+  };
 
-// Export for use
-window.firebaseApp = app;
-window.firestoreDB = db;
-window.firebaseAuth = auth;
-window.firebaseStorage = storage;
+  window.firebaseApp = initializeApp(firebaseConfig);
+  window.db = getFirestore(firebaseApp);
+  window.auth = getAuth(firebaseApp);
+</script>
